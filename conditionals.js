9f03e5a7694070476f293307019edd0b9fc83d5e
Gene Tenorlas <tenorlas@sheridancollege.ca>
2022-06-27 02:00:24 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -3,6 +3,9 @@ const cold = false;
 if (raining) {
   console.log("Don't forget your umbrella!");
 }
+if (!raining){
+  console.log("Leave your umbreall at home!");
+}
 /*
 if (cold){
   console.log("Make sure you pick out a scarf!");
@@ -19,4 +22,13 @@ if (temperature<0){
   console.log("Short sleeves are fine.");
 }
 
+const isCitizen = true;
+const age=26;
+if (isCitizen && age > 18){
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40){
+  console.log("Maybe going outside isn't such a great idea...");
+}
 console.log("Now you're ready to go outside!");
